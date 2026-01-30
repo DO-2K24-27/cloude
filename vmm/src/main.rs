270102130d063ee83ecbc23f4733668296d26c3d
Proxyfil <piloupilou200@gmail.com>
2026-01-30 21:54:15 +0100

    feat(vmm): Created first file to start vm

--- vmm/src/main.rs
+++ vmm/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::u32;
+use std::env;
+
+use vmm::VMM;
+
+#[derive(Debug)]
+pub enum Error {
+    VmmNew(vmm::Error),
+
+    VmmKernel(env::VarError),
+    
+    VmmConfigure(vmm::Error),
+
+    VmmRun(vmm::Error),
+}
+
+fn main() -> Result<(), Error> {
+
+    let mut vmm = VMM::new().map_err(Error::VmmNew)?;
+
+    let kernel_path = env::var("KERNEL_PATH").map_err(Error::VmmKernel)?;
+
+    vmm.configure(2, 4, &kernel_path)
+        .map_err(Error::VmmConfigure)?;
+
+    vmm.run();
+
+    Ok(())
 }
